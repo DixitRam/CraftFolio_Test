3889b580bac64b16354123b3344cb8bf7f48d1f3
hirax22machchh <hirax.machchh@gmail.com>
2025-03-02 23:46:27 +0530

    fetching data from database instead of fake data

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,11 +1,11 @@
 import { notFound } from "next/navigation";
 import ClientTemplate from "./clientTemplate";
 import data from "@/public/FakeData.json";
-
+import { getUserDetails } from "./connectDB";
 const users = data.users;
 
 export default async function userPortfolio({ params }: { params: { username: string } }) {
-    const user = users.find((u) => u.username === params.username);
+    const user = await getUserDetails(params.username);
     if (!user) return notFound();
 
     return <ClientTemplate user={user} templateName={user.template} />;
